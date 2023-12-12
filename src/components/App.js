4314432c44ea3caf5a870116204954fc33310647
Pranshu Agrawal <101993746+Pranshu-15@users.noreply.github.com>
2023-12-12 07:22:29 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count, setCount] = useState(0);
+  const countIncrease = () =>  {setCount(++count)};
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Button Clicked {count} times</h1>
+        <button onClick={countIncrease}>Click me</button>
     </div>
   )
 }
